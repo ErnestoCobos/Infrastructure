301c9a1faaa0740943405a18fa19bc5ccb35ac79
Ernesto Cobos <ernesto@cobos.io>
2022-06-06 00:25:01 +0000

    feat: fixed format

--- variables.tf
+++ variables.tf
@@ -1,32 +1,32 @@
 variable "region" {
-    description = "region"
-    type = string
+  description = "region"
+  type        = string
 }
 
 variable "vpc-one-name" {
-    description = "Name of the VPC"
-    type = string
+  description = "Name of the VPC"
+  type        = string
 }
 
 variable "vpc-one-description" {
-    description = "Description of the vpc"
-    type = string
+  description = "Description of the vpc"
+  type        = string
 }
 
 variable "vpn_instance_tags" {
-  type = list(string)
+  type        = list(string)
   description = "The tags of the VPN instance to create"
 }
 
 variable "vpn_instance_hostname" {
-  type = string
+  type        = string
   description = "The tags of the VPN instance to create"
 }
-variable "domain_one" { 
-    type = string
-    description = "The domain to create the record in"
+variable "domain_one" {
+  type        = string
+  description = "The domain to create the record in"
 }
 variable "ssh_key_ids" {
-    type = list(string)
-    description = "The SSH key IDs of the instance to create"
+  type        = list(string)
+  description = "The SSH key IDs of the instance to create"
 }
